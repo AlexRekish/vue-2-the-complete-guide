369b2a80a1b204c8248fcf82151ce97cea3b5ce6
Alexey Rekish <alexeyrekish@gmail.com>
2019-06-24 09:59:15 +0300

    exercise 6 done

---
+++ Exercise 6/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <app-header/>
+    <hr>
+    <div class="row">
+      <app-servers-list/>
+      <app-server-details/>
+    </div>
+    <hr>
+    <app-footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import ServersList from "./ServersList.vue";
+import ServerDetails from "./ServerDetails.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-servers-list": ServersList,
+    "app-server-details": ServerDetails,
+    "app-footer": Footer
+  }
+};
+</script>
+
+<style>
+</style>
